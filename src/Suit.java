58c29d823dc542e60ec3140f4b1c9f210caac2a3
John <johntay33lor@gmail.com>
2023-05-17 22:38:00 -0400

    Initial commit

---
+++ src/Suit.java
@@ -0,0 +1,16 @@
+public enum Suits {
+    CLUB("Clubs"),
+    DIAMOND("Diamonds"),
+    HEART("Hearts"),
+    SPADE("Spades");
+
+    String suitName;
+    Suits(String suitName) {
+        this.suitName = suitName;
+    }
+
+
+    public String toString() {
+        return suitName;
+    }
+}
